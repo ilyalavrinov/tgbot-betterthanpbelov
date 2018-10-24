c9539db472933fd5e70ceb517f38d60c344e6319
Ilya Lavrinov <ilya.v.lavrinov@gmail.com>
2018-10-24 16:10:35 +0300

    Merge fix

--- mybot/start.go
+++ mybot/start.go
@@ -28,18 +28,9 @@ func Start(cfg_filename string) error {
 	cron := tgbotbase.NewCron()
 
 	bot.AddHandler(tgbotbase.NewIncomingMessageDealer(cmd.NewPropertyHandler(propstorage)))
-<<<<<<< HEAD
 	bot.AddHandler(tgbotbase.NewIncomingMessageDealer(cmd.NewWeatherHandler(fullcfg.Weather.Token, redispool, propstorage)))
 	bot.AddHandler(tgbotbase.NewIncomingMessageDealer(cmd.NewRemindHandler(cron, remindstorage, propstorage)))
-=======
-<<<<<<< HEAD
-	bot.AddHandler(tgbotbase.NewIncomingMessageDealer(cmd.NewRemindHandler(cron, remindstorage)))
->>>>>>> 8bddb4f2c18ab5ed87e6c866e626614ad169ef20
 	bot.AddHandler(tgbotbase.NewBackgroundMessageDealer(cmd.NewKittiesHandler(cron, propstorage)))
-=======
-	bot.AddHandler(tgbotbase.NewIncomingMessageDealer(cmd.NewWeatherHandler(fullcfg.Weather.Token, redispool, propstorage)))
-	bot.AddHandler(tgbotbase.NewIncomingMessageDealer(cmd.NewRemindHandler(cron, remindstorage, propstorage)))
->>>>>>> ac6b42e6a926cc9ee4db23bfe6a69486b5d89a79
 	/*     handlers = append(handlers, cmd.NewKittiesHandler(),
 	       cmd.NewWeatherHandler(cfg.Weather.Token, opts),
 	       cmd.NewDeathHandler(),
